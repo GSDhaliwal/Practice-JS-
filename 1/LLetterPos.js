fc30c0c739678344619b76e2bbc0a2b46e3e237b
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-01-16 21:00:06 +0000

    lint

--- 1/LLetterPos.js
+++ 1/LLetterPos.js
@@ -13,14 +13,14 @@ const assertArrEq = function(actual, expected) {
 
 const letterPositions = function(sentence) {
   const results = {};
-    for (let index = 0; index < sentence.length; index++) {
-      if (sentence[index] !== " ") {
-        if (!results[sentence[index]]) {
-          results[sentence[index]] = [];
-        }
-        results[sentence[index]].push(index)
+  for (let index = 0; index < sentence.length; index++) {
+    if (sentence[index] !== " ") {
+      if (!results[sentence[index]]) {
+        results[sentence[index]] = [];
       }
+      results[sentence[index]].push(index);
     }
+  }
   return results;
 };
 
