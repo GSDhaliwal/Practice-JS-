6a9ca0571c8f45bb02d43c3bd8ddb9ee000057c8
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-01-28 22:54:21 +0000

    practice with array.map

---
+++ 4/mapPractice.js
@@ -0,0 +1,5 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+let lengthOfElements = lighthouses.map((e) => e.length)
+
+console.log(lengthOfElements)
