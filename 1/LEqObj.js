a464bdda2c1baa64c5c2bbb836fb04e39f3adbab
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-04-15 04:02:36 +0000

    added recursion to complete eqObj function

--- 1/LEqObj.js
+++ 1/LEqObj.js
@@ -17,7 +17,13 @@ const eqObj = function(object1, object2) {
     return false;
   }
   for (const key in object1) {
-    if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+    if (typeof object1[key] === "object" && object2[key]) {
+      let recursionResult = eqObj(object1[key], object2[key]);
+      return recursionResult 
+      //if (!recursionResult) {
+          //return false;
+        //}
+      } else if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
       return false;
     } else if (eqArr(object1[key], object2[key]) === false) {
       return false;
@@ -38,3 +44,11 @@ assertE(eqObj(ab, ba), true); //true
 assertE(eqObj(ab, abc), false); //true
 assertE(eqObj(cd, dc), true); //true
 assertE(eqObj(cd, cd2), true); //false
+
+assertE(eqObj({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+assertE(eqObj({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => true
+assertE(eqObj({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => true
+assertE(eqObj({ a: { y: 0, z: 1 }, b: 2 }, { b: 2, a: { y: 0, z: 1 }}), true) // => true
+
+assertE(eqObj({ a: { y: 0, z: { q: 1, f: 3 }}, b: 2 }, { b: 2, a: { z: { q: 1, f: 3 }, y: 0 }}), true) // => true
+assertE(eqObj({ a: { y: 0, z: { q: 1, f: 3 }}, b: 2 }, { b: 2, a: { z: { q: 1, f: 3 }, y: 1 }}), false) // => true
