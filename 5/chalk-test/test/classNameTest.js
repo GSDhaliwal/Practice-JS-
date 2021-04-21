b231fd52c0ba71b8e6fed4d3e13af0ea7a2f166b
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-04-21 23:36:20 +0000

    mocha/chai practice

---
+++ 5/chalk-test/test/classNameTest.js
@@ -0,0 +1,31 @@
+let addClass = require('../js/className')
+let chai = require('chai');
+let assert = chai.assert;
+
+describe('addClass', function() {
+  it('should add class to element', function () {
+    let element  = { className: ""}
+
+    addClass(element, 'test-class');
+
+    assert.equal(element.className, 'test-class')
+  });
+  
+  it('should not add a class which already exists', function () {
+    let element  = { className: "exists"}
+
+    addClass(element, 'exists');
+
+    var numClasses = element.className.split(" ").length;
+    assert.equal(numClasses, 1);
+  })
+
+  it('should append new class after existing one', function() {
+    var element = { className: 'exists' };
+  
+    addClass(element, 'new-class');
+  
+    var classes = element.className.split(' ');
+    assert.equal(classes[1], 'new-class');
+  });
+});
