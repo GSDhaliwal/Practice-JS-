922a094c7f57ec1ddb2e7e219028284503740b32
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-04-25 19:50:13 +0000

    fixed practice caeser cipher

--- 5/caesarCipher/caesarCipher.js
+++ 5/caesarCipher/caesarCipher.js
@@ -1,22 +1,16 @@
 const encrypt = function(plaintext, key) {
   let cipheredResult = "";
   for (let text = 0; text < plaintext.length; text++) {
-    if (plaintext.charCodeAt(plaintext[text]) >= 97 && plaintext.charCodeAt(plaintext[text]) <= 122){
-      if (typeof key[0] !== "integer") {
-        if (plaintext.charCodeAt(plaintext[text]) + key < 97 ) {
-          let numbersToLoopAround = 97 - (plaintext.charCodeAt(plaintext[text]) + key)
-          cipheredResult += String.fromCharCode(123 - numbersToLoopAround);
-        } else if (plaintext.charCodeAt(plaintext[text]) + key > 97) {
-          cipheredResult += String.fromCharCode(plaintext.charCodeAt(plaintext[text]) + key);
+    if (plaintext.charCodeAt(text) >= 97 && plaintext.charCodeAt(text) <= 122){
+      if (plaintext.charCodeAt(text) + key < 97 ) {
+        let numbersToLoopAround = 97 - (plaintext.charCodeAt(text) + key)
+        cipheredResult += String.fromCharCode(123 - numbersToLoopAround);
+      } else if (plaintext.charCodeAt(text) + key > 122 ) {
+        let numbersToLoopAround = (plaintext.charCodeAt(text) + key) - 122;
+        cipheredResult += String.fromCharCode(96 + numbersToLoopAround);
+      } else {
+        cipheredResult += String.fromCharCode(plaintext.charCodeAt(text) + key);
         };
-      } else {      
-        if (plaintext.charCodeAt(plaintext[text]) + key > 122 ) {
-          let numbersToLoopAround = (plaintext.charCodeAt(plaintext[text]) + key) - 122;
-          cipheredResult += String.fromCharCode(96 + numbersToLoopAround);
-        } else if (plaintext.charCodeAt(plaintext[text]) + key < 122) {
-          cipheredResult += String.fromCharCode(plaintext.charCodeAt(plaintext[text]) + key);
-        };
-      };
     } else {
       cipheredResult += plaintext[text];
     }
