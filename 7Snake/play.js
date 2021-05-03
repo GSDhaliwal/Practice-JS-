491f685eb34cc20fceb954f202a7ff690c990925
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-05-03 21:47:34 +0000

    send data (intials) to snake server

--- 7Snake/play.js
+++ 7Snake/play.js
@@ -1,27 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //'connect' event is triggered as soon as connection with server is established
-  conn.on("connect", () => {
-    console.log("connected to snake server")
-  });
-
-  //data event is triggered when server sends data to client
-  conn.on("data", (data) => {
-    console.log(data)
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
