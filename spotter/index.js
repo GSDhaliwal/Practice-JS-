b85043afcf2868ddb059d41c5a2a96961fffbd06
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-05-13 22:35:37 +0000

    use API to retireve IP address

---
+++ spotter/index.js
@@ -0,0 +1,14 @@
+const { fetchMyIP } = require('./iss');
+
+/*
+fetchMyIP((error, ip) => {
+
+  if (error) {
+    console.log('There was an error in the request:\n', error);
+  }
+
+  if (ip) {
+    console.log('It worked! Return IP:',ip);
+  }
+})
+*/
