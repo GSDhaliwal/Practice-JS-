157d5e9292783158f934122841eb65fc9b0795ee
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2021-06-09 21:31:51 +0000

    filling out urls_index.ejs template

--- PTApp/server.js
+++ PTApp/server.js
@@ -9,7 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
-
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -20,6 +19,11 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
